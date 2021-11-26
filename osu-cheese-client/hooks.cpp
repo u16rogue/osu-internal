7cd25dae60ba6b87cf086e3653886f2d862a85ca
u16rogue <u16rogue@waifu.club>
2021-11-26 21:53:25 +0800

    clean up code for CallWindowProc

--- osu-cheese-client/hooks.cpp
+++ osu-cheese-client/hooks.cpp
@@ -2,11 +2,10 @@
 
 #include <Windows.h>
 #include <windowsx.h>
-#include <intrin.h>
 #include <sed/console.hpp>
 #include <sed/memory.hpp>
 
-static auto CALLBACK hk_CallWindowProc(WNDPROC lpPrevWndFunc, HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam) -> void
+static auto CALLBACK CallWindowProc_hk(WNDPROC lpPrevWndFunc, HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam) -> void
 {
 	if (Msg == WM_LBUTTONDOWN)
 	{
@@ -14,9 +13,10 @@ static auto CALLBACK hk_CallWindowProc(WNDPROC lpPrevWndFunc, HWND hWnd, UINT Ms
 	}
 }
 
-void * target_CallWindowProc = CallWindowProcW;
+void * CallWindowProc_target = CallWindowProcW;
 
-static auto __attribute__((naked)) trampoline_CallWindowProc(WNDPROC lpPrevWndFunc, HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam) -> LRESULT
+// osu! detects hooks by checking the return address, this little trampoline prevents that.
+static auto __attribute__((naked)) CallWindowProc_trampoline(WNDPROC lpPrevWndFunc, HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam) -> LRESULT
 {
 	__asm
 	{
@@ -27,29 +27,48 @@ static auto __attribute__((naked)) trampoline_CallWindowProc(WNDPROC lpPrevWndFu
 		push [ebp+16]
 		push [ebp+12]
 		push [ebp+8] // forgot the return lol
-		call hk_CallWindowProc
-		mov eax, target_CallWindowProc
+		call CallWindowProc_hk
+		mov eax, CallWindowProc_target
 		lea eax, [eax + 5]
 		jmp eax
 	}
+}
+
+static auto CallWindowProc_install() -> bool
+{
+	std::uint8_t CallWindowProc_jmp_shellcode[] = { 0xE9, 0x00, 0x00, 0x00, 0x00 };
+
+	printf("\n[+] Installing CallWindowProc");
+
+	printf("\n[+] Changing CallWindowProc_target protection...");
+	DWORD oprot { 0 };
+	if (!VirtualProtect(CallWindowProc_target, sizeof(CallWindowProc_jmp_shellcode), PAGE_EXECUTE_READWRITE, &oprot))
+		return false;
+
+	auto rel = sed::abs2rel32(CallWindowProc_target, sizeof(CallWindowProc_jmp_shellcode), CallWindowProc_trampoline);
+	printf("\n[+] Calculating CallWindowProc_target -> CallWindowProc_trampoline relative address... ABS: 0x%p REL: 0x%p", CallWindowProc_trampoline, rel);
+	*reinterpret_cast<std::uintptr_t*>(CallWindowProc_jmp_shellcode + 0x1) = rel;
+
+	printf("\n[+] Patching CallWindowProc_trampoline jump...");
+	std::memcpy(CallWindowProc_target, CallWindowProc_jmp_shellcode, sizeof(CallWindowProc_jmp_shellcode));
+
+	printf("\n[+] Verifying patch...");
+	if (std::memcmp(CallWindowProc_target, CallWindowProc_jmp_shellcode, sizeof(CallWindowProc_jmp_shellcode)))
+		return false;
+
+	printf("\n[+] Restoring CallWindowProc_target protection...");
+	if (!VirtualProtect(CallWindowProc_target, sizeof(CallWindowProc_jmp_shellcode), oprot, &oprot))
+		return false;
+
+	return true;
 }
 
 auto hooks::install() -> bool
 {
 	printf("\n[+] Installing hooks...");
 	
-	std::uint8_t cwpa_sc[] = { 0xE9, 0x00, 0x00, 0x00, 0x00 };
+	if (!CallWindowProc_install())
+		return false;
 
-	printf("\n[+] Changing protection...");
-	DWORD oprot { 0 };
-	VirtualProtect(target_CallWindowProc, sizeof(cwpa_sc), PAGE_EXECUTE_READWRITE, &oprot);
-
-	auto rel = sed::abs2rel32(target_CallWindowProc, sizeof(cwpa_sc), trampoline_CallWindowProc);
-	printf("\n[+] Calculating relative address... ABS: 0x%p REL: 0x%p", trampoline_CallWindowProc, rel);
-	*reinterpret_cast<std::uintptr_t*>(cwpa_sc + 0x1) = rel;
-
-	printf("\n[+] Patching jump...");
-	std::memcpy(target_CallWindowProc, cwpa_sc, sizeof(cwpa_sc));
-	
 	return true;
 }
